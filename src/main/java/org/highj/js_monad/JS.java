181b684a8df6f4c8c189800d352939c68519c9da
Clinton Selke <clinuxrulz@gmail.com>
2016-08-21 20:46:46 +1000

    pure / bind

--- src/main/java/org/highj/js_monad/JS.java
+++ src/main/java/org/highj/js_monad/JS.java
@@ -4,8 +4,9 @@ import org.derive4j.Data;
 import org.derive4j.Derive;
 import org.derive4j.Flavour;
 import org.derive4j.Visibility;
+import org.highj.function.F1;
 
-@Data(value = @Derive(inClass = "FJImpl", withVisibility = Visibility.Package), flavour = Flavour.HighJ)
+@Data(value = @Derive(inClass = "JSImpl", withVisibility = Visibility.Package), flavour = Flavour.HighJ)
 public abstract class JS<A> {
 
     public interface Cases<R,A> {
@@ -14,4 +15,24 @@ public abstract class JS<A> {
     }
 
     public abstract <R> R match(Cases<R,A> cases);
+
+    public static <A> JS<A> pure(A a) {
+        return JSImpl.Pure(a);
+    }
+
+    public <B> JS<B> map(F1<A,B> f) {
+        return this.bind((A a) -> pure(f.apply(a)));
+    }
+
+    public <B> JS<B> bind(F1<A,JS<B>> f) {
+        return JSImpl
+            .<A>cases()
+            .Pure(f)
+            .Bind((Bound<?,A> bound) -> bindBound(bound, f))
+            .apply(this);
+    }
+
+    private static <A,B,C> JS<C> bindBound(Bound<A,B> bound, F1<B,JS<C>> f) {
+        return JSImpl.Bind(new Bound<>(bound.ma, (A a) -> bound.f.apply(a).bind(f)));
+    }
 }
