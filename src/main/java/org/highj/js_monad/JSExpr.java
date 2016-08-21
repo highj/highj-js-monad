14e430688783987e365f7908191b81a93fda66f9
Clinton Selke <clinuxrulz@gmail.com>
2016-08-21 21:17:50 +1000

    JsExprId and basic JSExpr

--- src/main/java/org/highj/js_monad/JSExpr.java
+++ src/main/java/org/highj/js_monad/JSExpr.java
@@ -1,4 +1,55 @@
 package org.highj.js_monad;
 
-public class JSExpr {
+import org.derive4j.Data;
+import org.derive4j.Derive;
+import org.derive4j.Flavour;
+import org.derive4j.Visibility;
+
+@Data(value = @Derive(inClass = "JSExprImpl", withVisibility = Visibility.Package), flavour = Flavour.HighJ)
+public abstract class JSExpr {
+
+    public interface Cases<R> {
+        R LitString(String stringValue);
+        R AppendString(JSExprId e1, JSExprId e2);
+    }
+
+    public abstract <R> R match(Cases<R> cases);
+
+    public static JSExpr litString(String a) {
+        return JSExprImpl.LitString(a);
+    }
+
+    public static JSExpr appendString(JSExprId e1, JSExprId e2) {
+        return JSExprImpl.AppendString(e1, e2);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) { return false; }
+        if (!(obj instanceof JSExpr)) { return false; }
+        JSExpr other = (JSExpr)obj;
+        return JSExprImpl
+            .cases()
+            .LitString((String a1) -> JSExprImpl.cases().LitString((String a2) -> a1.equals(a2)).otherwise(false))
+            .AppendString((JSExprId e11, JSExprId e12) -> JSExprImpl.cases().AppendString((JSExprId e21, JSExprId e22) -> e11.equals(e21) && e12.equals(e22)).otherwise(false))
+            .apply(this)
+            .apply(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return match(HashCases.instance);
+    }
+
+    private static class HashCases implements Cases<Integer> {
+        public static final HashCases instance = new HashCases();
+        @Override
+        public Integer LitString(String stringValue) {
+            return stringValue.hashCode();
+        }
+        @Override
+        public Integer AppendString(JSExprId e1, JSExprId e2) {
+            return 1 ^ e1.hashCode() ^ e2.hashCode();
+        }
+    }
 }
--- src/main/java/org/highj/js_monad/JSExprId.java
+++ src/main/java/org/highj/js_monad/JSExprId.java
@@ -1,4 +1,30 @@
 package org.highj.js_monad;
 
 public class JSExprId {
+    private final int _id;
+
+    private JSExprId(int id) {
+        this._id = id;
+    }
+
+    public static JSExprId of(int id) {
+        return new JSExprId(id);
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) { return false; }
+        if (!(obj instanceof JSExprId)) { return false; }
+        JSExprId other = (JSExprId)obj;
+        return _id == other._id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(_id);
+    }
 }
